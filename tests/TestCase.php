15c2c1259266187c8e3e7db8ba725ac3b28730d1
DGINXREAL <DGINXREAL@users.noreply.github.com>
2024-06-16 08:22:50 +0000

    Fix styling

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -4,6 +4,7 @@ namespace DGINXREAL\FilamentIframePlugin\Tests;
 
 use BladeUI\Heroicons\BladeHeroiconsServiceProvider;
 use BladeUI\Icons\BladeIconsServiceProvider;
+use DGINXREAL\FilamentIframePlugin\FilamentIframePluginServiceProvider;
 use Filament\Actions\ActionsServiceProvider;
 use Filament\FilamentServiceProvider;
 use Filament\Forms\FormsServiceProvider;
@@ -16,7 +17,6 @@ use Illuminate\Database\Eloquent\Factories\Factory;
 use Livewire\LivewireServiceProvider;
 use Orchestra\Testbench\TestCase as Orchestra;
 use RyanChandler\BladeCaptureDirective\BladeCaptureDirectiveServiceProvider;
-use DGINXREAL\FilamentIframePlugin\FilamentIframePluginServiceProvider;
 
 class TestCase extends Orchestra
 {
